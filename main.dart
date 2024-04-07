8c81c706a543d572da9c5f88988118e2b0260eb6
ParvizMammadov <parviz-mamedov-93@mail.ru>
2024-04-08 01:57:19 +0400

    first commit

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'model/rectangle_model.dart';
+
+void main() {
+  Rectangle perimetr = Rectangle(
+    width: 3.45,
+    heigh: 4.56,
+  );
+
+  print('${perimetr.heigh}, ${perimetr.width}');
+}
---
+++ model/rectangle_model.dart
@@ -0,0 +1,8 @@
+class Rectangle {
+  double? width;
+  double? heigh;
+  Rectangle({this.width, this.heigh});
+  void showInfo() {
+    print('heigh:$heigh,width:$width');
+  }
+}
